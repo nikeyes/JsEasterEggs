d3cd62bcfb88dc8dcbed4dadf1abb5af1e57560c
Jorge Castro <nikey_es@yahoo.es>
2015-11-16 01:18:47 +0100

    firts sample

--- JsSnow/jsSnow.js
+++ JsSnow/jsSnow.js
@@ -14,18 +14,16 @@ window.onload = function(){
 		
 	document.body.insertBefore(canvas, document.body.firstChild);
 	
-	//canvas dimensions
-	
 	//snowflake particles
-	var maxSnowflakes = 100; //max particles
+	var maxSnowflakes = 1000; //max particles
 	var snowflakes = [];
 	for(var i = 0; i < maxSnowflakes; i++)
 	{
 		snowflakes.push({
-			x: Math.random() * maxWidth, //x-coordinate
-			y: Math.random() * maxHeight, //y-coordinate
-			r: Math.random() * 4 + 1, //radius
-			d: Math.random() * maxSnowflakes //density
+			x: Math.random() * maxWidth,
+			y: Math.random() * maxHeight, 
+			radius: Math.random() * 5,
+			density: Math.random() * maxSnowflakes
 		})
 	}
 	
@@ -38,9 +36,9 @@ window.onload = function(){
 		ctx.beginPath();
 		for(var i = 0; i < maxSnowflakes; i++)
 		{
-			var p = snowflakes[i];
-			ctx.moveTo(p.x, p.y);
-			ctx.arc(p.x, p.y, p.r, 0, Math.PI*2, true);
+			var snowflake = snowflakes[i];
+			ctx.moveTo(snowflake.x, snowflake.y);
+			ctx.arc(snowflake.x, snowflake.y, snowflake.radius, 0, Math.PI * 2, true);
 		}
 		ctx.fill();
 		update();
@@ -59,7 +57,7 @@ window.onload = function(){
 			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
 			//Every particle has its own density which can be used to make the downward movement different for each flake
 			//Lets make it more random by adding in the radius
-			p.y += Math.cos(angle+p.d) + 1 + p.r/2;
+			p.y += Math.cos(angle + p.density) + 1 + p.radius / 2;
 			p.x += Math.sin(angle) * 2;
 			
 			//Sending flakes back from the top when it exits
@@ -68,7 +66,7 @@ window.onload = function(){
 			{
 				if(i%3 > 0) //66.67% of the flakes
 				{
-					snowflakes[i] = {x: Math.random() * maxWidth, y: -10, r: p.r, d: p.d};
+					snowflakes[i] = {x: Math.random() * maxWidth, y: -10, radius: p.radius, density: p.density};
 				}
 				else
 				{
@@ -76,12 +74,12 @@ window.onload = function(){
 					if(Math.sin(angle) > 0)
 					{
 						//Enter from the left
-						snowflakes[i] = {x: -5, y: Math.random() * maxHeight, r: p.r, d: p.d};
+						snowflakes[i] = {x: -5, y: Math.random() * maxHeight, radius: p.radius, density: p.density};
 					}
 					else
 					{
 						//Enter from the right
-						snowflakes[i] = {x: maxWidth + 5, y: Math.random() * maxHeight, r: p.r, d: p.d};
+						snowflakes[i] = {x: maxWidth + 5, y: Math.random() * maxHeight, radius: p.radius, density: p.density};
 					}
 				}
 			}
