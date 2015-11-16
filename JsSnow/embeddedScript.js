85c7760699690bf62e1738388a543c8f2c8fed4e
Jorge Castro <nikey_es@yahoo.es>
2015-11-17 00:35:47 +0100

    minor fixes

--- JsSnow/embeddedScript.js
+++ JsSnow/embeddedScript.js
@@ -1,1 +1,1 @@
-var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//nikeyes.github.io/JsEasterEggs/JsSnow/jsSnow.js';void(0); 
+javascript:var s = document.createElement('script');s.type='text/javascript';document.body.appendChild(s);s.src='//nikeyes.github.io/JsEasterEggs/JsSnow/jsSnow.js';void(0); 
