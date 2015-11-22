10fe5cafccc8fa416f7a6b2b19b5ca736307d9a7
Jorge Castro <nikey_es@yahoo.es>
2015-11-22 16:03:46 +0100

    Added WindFactor

--- JsSnow/src/embeddedScript.js
+++ JsSnow/src/embeddedScript.js
@@ -2,7 +2,8 @@
 	maxHeight: 575,
 	maxWidth:200,
 	snowflakesNumber: 200,
-	fps: 30
+	fps: 30,
+	windFactor: 1
 };*/ 
 var script = document.createElement('script');
 script.type='text/javascript';document.body.appendChild(script);
--- JsSnow/src/jsSnow.js
+++ JsSnow/src/jsSnow.js
@@ -1,4 +1,7 @@
-/*Based on: http://thecodeplayer.com/walkthrough/html5-canvas-snow-effect*/
+/*Based on: 
+http://thecodeplayer.com/walkthrough/html5-canvas-snow-effect
+http://jumptofive.com/canvas-como-crear-efecto-de-nieve-cayendo/
+*/
 (function () {
     "use strict";
 	
@@ -16,9 +19,12 @@
 		__canvas,
 		__ctx,
 		__maxSnowflakes,
+		__maxSizeSnowflake,
 	    __snowflakes = [],
 		__fps,
-		__interval;
+		__interval,
+		__horizontalSwingFactor,
+        __windFactor;
 		
 	var JsSnow = function (options) {
 		//Default Options
@@ -29,6 +35,9 @@
 		__resizeWidth = true;
 		__resizeHeight = true;
 		__maxSnowflakes = 500;
+		__maxSizeSnowflake = 5;
+		__horizontalSwingFactor = 2;
+        __windFactor = 1;
 		
 		__configureOptions.call(this, options);
 		__createCanvas.call(this);
@@ -38,7 +47,7 @@
 		/*To simplify the code I use setInterval instead of requestAnimationFrame
 		You can view the changes in this commit: 
 		"replace setInterval with requestAnimationFrame(__draw);"*/
-		setInterval(__draw, __interval);
+		setInterval(__drawSnowflakes, __interval);
 	};
 
 	var __configureOptions = function (options) {
@@ -80,6 +89,13 @@
 			__maxSnowflakes = window.JsSnowOptions.snowflakesNumber;
 		} 
 		
+		//Configure Winf Factor
+		if (options && options.windFactor) {
+			__windFactor = options.windFactor;
+		}
+		else if (window.JsSnowOptions && window.JsSnowOptions.windFactor) {
+			__windFactor = window.JsSnowOptions.windFactor;
+		} 
 	};
 	
 	var __createCanvas = function () {
@@ -114,13 +130,13 @@
 			__snowflakes.push({
 				x: Math.random() * __maxWidth,
 				y: Math.random() * __maxHeight, 
-				radius: Math.random() * 5,
+				radius: Math.random() * __maxSizeSnowflake,
 				density: Math.random() * __maxSnowflakes
 			});
 		}
 	};
 	
-	var __draw = function () {
+	var __drawSnowflakes = function () {
 			__ctx.clearRect(0, 0, __maxWidth, __maxHeight);
 		
 			__ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
@@ -133,24 +149,22 @@
 			}
 			
 			__ctx.fill();
-			__update.call(this);	
+			__updateSnowflakesPosition.call(this);	
 			
 	};
 	
 	var angle = 0;
-	var __update = function () {
+	var __updateSnowflakesPosition = function () {
 		angle += 0.01;
 		for(var i = 0; i < __maxSnowflakes; i++)
 		{
 			var p = __snowflakes[i];
 			
-			//angle will be an ongoing incremental flag. Sin and Cos functions will be applied to it to create vertical and horizontal movements of the flakes
-			//Updating X and Y coordinates
 			//We will add 1 to the cos function to prevent negative values which will lead flakes to move upwards
 			//Every particle has its own density which can be used to make the downward movement different for each flake
 			//Lets make it more random by adding in the radius
-			p.y += Math.cos(angle + p.density) + 1 + p.radius / 2;
-			p.x += Math.sin(angle) * 2;
+			p.x += Math.sin(angle) * __horizontalSwingFactor + __windFactor;
+			p.y += ((Math.cos(angle + p.density) + 1) + p.radius / 2);
 			
 			//Sending flakes back from the top when it exits
 			//Lets make it a bit more organic and let flakes enter from the left and right also.
@@ -158,7 +172,8 @@
 			{
 				if(i%3 > 0) //66.67% of the flakes
 				{
-					__snowflakes[i] = {x: Math.random() * __maxWidth, y: -10, radius: p.radius, density: p.density};
+					__snowflakes[i].x = Math.random() * __maxWidth;
+					__snowflakes[i].y = -10;
 				}
 				else
 				{
@@ -166,13 +181,14 @@
 					if(Math.sin(angle) > 0)
 					{
 						//Enter from the left
-						__snowflakes[i] = {x: -5, y: Math.random() * __maxHeight, radius: p.radius, density: p.density};
+						__snowflakes[i].x = -5;
 					}
 					else
 					{
 						//Enter from the right
-						__snowflakes[i] = {x: __maxWidth + 5, y: Math.random() * __maxHeight, radius: p.radius, density: p.density};
+						__snowflakes[i].x = __maxWidth + 5;
 					}
+					__snowflakes[i].y = Math.random() * __maxHeight;
 				}
 			}
 		}
